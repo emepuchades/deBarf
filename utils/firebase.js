08e10f03f5afe5eca4f7283cee3f95f6f74d90a1
Puchi <maria.puchades9@gmail.com>
2022-09-10 12:21:54 +0200

    Refactor and resolve bug of Async Storage in firebase

--- utils/firebase.js
+++ utils/firebase.js
@@ -1,27 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 import {
-  FIREBASE_APIKEY,
-  FIREBASE_AUTHDOMAIN,
-  FIREBASE_PROJECTID,
-  FIREBASE_STORAGEBUCKET,
-  FIREBASE_MESSAGINGSENDERID,
-  FIREBASE_APPID,
-  FIREBASE_MEASUREMENTID
-} from 'react-native-dotenv';
+  initializeAuth,
+  getReactNativePersistence
+} from 'firebase/auth/react-native';
 
+// add firebase config here
 const firebaseConfig = {
-  apiKey: FIREBASE_APIKEY,
-  authDomain: FIREBASE_AUTHDOMAIN,
-  projectId: FIREBASE_PROJECTID,
-  storageBucket: FIREBASE_STORAGEBUCKET,
-  messagingSenderId: FIREBASE_MESSAGINGSENDERID,
-  appId: FIREBASE_APPID,
-  measurementId: FIREBASE_MEASUREMENTID
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTHDOMAIN,
+  projectId: process.env.FIREBASE_PROJECTID,
+  storageBucket: process.env.FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+  appId: process.env.FIREBASE_APPID,
+  measurementId: process.env.FIREBASE_MEASUREMENTID
 };
+// initialize firebase app
+const app = initializeApp(firebaseConfig);
 
-initializeApp(firebaseConfig);
-export const auth = getAuth();
+// initialize auth
+export const auth = initializeAuth(app, {
+  persistence: getReactNativePersistence(AsyncStorage)
+});
+
 export const database = getFirestore();
-
