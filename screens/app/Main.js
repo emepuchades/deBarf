7477e7ac896bab4b3f46e2319e46f578e3717792
Puchi <maria.puchades9@gmail.com>
2022-09-18 23:38:52 +0200

    Added multilenguages

--- screens/app/Main.js
+++ screens/app/Main.js
@@ -6,11 +6,15 @@ import { AntDesign } from '@expo/vector-icons';
 
 import Home from './Home/Home';
 import AddPost from './AddPost/AddPost';
+import ProfileScreen from './Profile/Profile'
 import colors from '../../utils/colors';
+import { useTranslation } from 'react-i18next';
 
 const Tab = createBottomTabNavigator();
 
 const MyTabs = () => {
+    const { t } = useTranslation();
+
     return (
         <Tab.Navigator
             initialRouteName="Mascotas"
@@ -25,35 +29,35 @@ const MyTabs = () => {
                 tabBarActiveTintColor: colors.navHover,
             }}
         >
-            <Tab.Screen name="Mascotas" component={Home}
+            <Tab.Screen name={t('navBottom.pets')} component={Home}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="dog" color={color} size={26} />
+                        <MaterialCommunityIcons name="dog" color={color} size={size} />
                     ),
                 }} />
-            <Tab.Screen name="Comunidad" component={AddPost}
+            <Tab.Screen name={t('navBottom.community')} component={AddPost}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="contacts-outline" color={color} size={26} />
+                        <MaterialCommunityIcons name="contacts-outline" color={color} size={size} />
                     ),
                 }} />
 
-            <Tab.Screen name="Publicar" component={AddPost}
+            <Tab.Screen name={t('navBottom.newPost')} component={AddPost}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <AntDesign name="plus" color={color} size={26} />
+                        <AntDesign name="plus" color={color} size={size} />
                     ),
                 }} />
-            <Tab.Screen name="Alimentos" component={Home}
+            <Tab.Screen name={t('navBottom.foods')} component={Home}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="food-drumstick-outline" color={color} size={26} />
+                        <MaterialCommunityIcons name="food-drumstick-outline" color={color} size={size} />
                     ),
                 }} />
-            <Tab.Screen name="Perfil" component={Home}
+            <Tab.Screen name={t('navBottom.profile')} component={ProfileScreen}
                 options={{
                     tabBarIcon: ({ color, size }) => (
-                        <MaterialCommunityIcons name="account-circle-outline" color={color} size={26} />
+                        <MaterialCommunityIcons name="account-circle-outline" color={color} size={size} />
                     ),
                 }} />
         </Tab.Navigator>
