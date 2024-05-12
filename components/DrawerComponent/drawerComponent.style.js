cc6c0220c40573a48aef3c4da7280f01012359c2
María Puchades <100128850+emepuchades@users.noreply.github.com>
2024-05-12 18:04:06 +0200

    feat: update styles in drawer

--- components/DrawerComponent/drawerComponent.style.js
+++ components/DrawerComponent/drawerComponent.style.js
@@ -1,39 +1,42 @@
 import colors from '../../utils/colors'
 
 export const styleDrawer = {
-    imageProfile: {
-        height: 80,
-        width: 80,
-        borderRadius: 40,
-        marginBottom: 10 
-    },
-    textUsername: {
-        color: colors.primary,
-        fontSize: 18,
-        marginBottom: 5,
-    },
-    imageBackground: {
-        padding: 20
-    },
-    listNavigation: {
-        flex: 1,
-        backgroundColor: colors.primary,
-        paddingTop: 20
-    },
-    navBottom: {
-        padding: 20,
-        borderTopWidth: 1,
-        borderTopColor: colors.grey
-    },
-    textSignOut: {
-        fontSize: 15,
-        marginLeft: 5,
-    },
-    containerSignOut: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    }, 
-    buttons: {
-        paddingVertical: 15
-    }
-}
+  imageProfile: {
+    height: 80,
+    width: 80,
+    borderRadius: 40,
+    marginBottom: 10,
+  },
+  textUsername: {
+    color: colors.primary,
+    fontSize: 18,
+    marginBottom: 5,
+  },
+  imageBackground: {
+    padding: 20,
+  },
+  listNavigation: {
+    flex: 1,
+    backgroundColor: colors.primary,
+    paddingTop: 20,
+  },
+  navBottom: {
+    padding: 20,
+    borderTopWidth: 1,
+    borderTopColor: colors.grey,
+  },
+  textSignOut: {
+    fontSize: 15,
+    marginLeft: 5,
+  },
+  containerSignOut: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  buttons: {
+    paddingVertical: 15,
+  },
+  listNavigaton: {
+    paddingTop: 35,
+  }
+};
